7e778f6354bd4a915eee634c3362242687655021
Nicholas Hazel <nicholas.hazel@gmail.com>
2020-11-29 20:51:34 -0800

    feat: Scaffolding in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,43 @@
+// Core
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routing / Supplemental
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/scaffold/header/header.component';
+import { ShoppingListComponent } from './components/shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping/shopping-list-edit/shopping-list-edit.component';
+import { IngredientComponent } from './components/shopping/ingredient/ingredient.component';
+import { RecipeBookComponent } from './components/recipe/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './components/recipe/recipe/recipe.component';
+import { FooterComponent } from './components/scaffold/footer/footer.component';
+import { RecipePageComponent } from './components/recipe/recipe.component'
+import { ShoppingPageComponent } from './components/shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+
+    RecipePageComponent,
+    ShoppingPageComponent,
+
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    IngredientComponent,
+
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/recipe/recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-page',
+  templateUrl: './recipe.component.html',
+  styleUrls: ['./recipe.component.scss']
+})
+export class RecipePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
